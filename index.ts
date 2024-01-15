3b24ee1adfab1cc9b8f7bd33258325175e64aab1
Arianny Mendoza <132972387+Ariannymm@users.noreply.github.com>
2024-01-15 08:16:42 -0400

    Desactivando politicas de seguridad de vercel, agregue los headers necesarios para el cors

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import loginRouter from "./routes/login";
 import cardsRouter from "./routes/cards";
@@ -8,12 +7,6 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3100;
-
-// middleware
-app.use(cors({
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['*'],
-  }));
 
 app.use(express.json());
 
