40f4f119188986f4ea4f9144232128bdca6bc2af
Arianny Mendoza <132972387+Ariannymm@users.noreply.github.com>
2024-01-14 23:35:45 -0400

    Configurando cors directamente en index.ts

--- index.ts
+++ index.ts
@@ -13,8 +13,10 @@ const port = process.env.PORT || 3100;
 app.use(
     cors({
         origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],  
     })
 );
+
 app.use(express.json());
 
 app.use('/api/login', loginRouter);
